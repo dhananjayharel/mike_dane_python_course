972d101ab2e92482d43eefa2d6e9f9bebc50bebe
dhananjay <harel.dhananjay@gmail.com>
2019-11-11 15:50:33 +0530

    added some examples

---
+++ strings/main.py
@@ -0,0 +1,10 @@
+greeting = "Hello"
+#indexes:   01234
+
+print( len(greeting) )
+print( greeting[0] )
+print( greeting[-1] )
+print( greeting.find("llo") )
+print( greeting.find("z") )
+print( greeting[2:] )
+print( greeting[2:3] )
